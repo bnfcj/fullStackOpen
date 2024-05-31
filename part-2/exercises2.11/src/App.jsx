c8a3b820fe613b6c72bfa0d3847e8ce51390de7e
bnfcj <67569485+bnfcj@users.noreply.github.com>
2024-05-31 13:39:19 +0800

    2.11 done

--- part-2/exercises2.11/src/App.jsx
+++ part-2/exercises2.11/src/App.jsx
@@ -1,16 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { SearchComponent } from "./components/SearchComponent";
 import Form from "./components/Form";
 import Heading2 from "./components/Heading2";
 import Addresses from "./components/Addresses";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then((response) => setPersons(response.data));
+  }, []);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filteredName, setFilteredName] = useState("");
